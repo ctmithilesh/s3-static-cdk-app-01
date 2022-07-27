c17409c4f484951facf33375cafe53bdce4c6820
morbiustech <mithilesht@cloudthat.com>
2022-07-27 17:20:49 +0530

    init commit

--- lib/s3-static-cdk-app-01-stack.ts
+++ lib/s3-static-cdk-app-01-stack.ts
@@ -1,19 +1,23 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as s3Deployment from 'aws-cdk-lib/aws-s3-deployment'
 import { Construct } from 'constructs';
 
 export class S3StaticCdkApp01Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'S3StaticCdkApp01Queue', {
-      visibilityTimeout: Duration.seconds(300)
+    const pandasWebsiteBucket = new s3.Bucket(this, 'pandasWebsiteBucket', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+    });
+    
+    new s3Deployment.BucketDeployment(this, 'pandasWebsiteBucket', {
+      sources: [s3Deployment.Source.asset('./website')],
+      destinationBucket: pandasWebsiteBucket,
+     //destinationKeyPrefix: 'web/static', // optional prefix in destination bucket
     });
 
-    const topic = new sns.Topic(this, 'S3StaticCdkApp01Topic');
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
